7e4a6e059b0c61b8b3ebcbde4aa21ca806300f8f
PhanNgocTrieu <47733751+PhanNgocTrieu@users.noreply.github.com>
2021-06-05 00:23:05 +0700

    Update main.cpp

--- Class/String/main.cpp
+++ Class/String/main.cpp
@@ -20,35 +20,32 @@ int main()
 
 	std::cout << std::endl << "S4 - OUTPUT: " << std::endl;
 	String s4{ "scasasd_4" };
+	std::cout << "s4: " << s4;
 	s3.insert(3, s4);
 	std::cout << s3;
-	//String s5(const_cast<char*>("b"));
-	/*String s4(const_cast<char*>("a"));
-	
-	String s6(const_cast<char*>("a"));
 
-	if (s4 < s5)
-	{
-		std::cout << "a < b!" << std::endl;
-	}
-	if (s5 > s4)
-	{
-		std::cout << "a > b!" << std::endl;
-	}
-	if (s4 == s6)
-	{
-		std::cout << "a == a!" << std::endl;
-	}*/
+
+	std::cout << "************** SubStr ***************" << std::endl;
+	String s5;
+	s5 = s3.substr(3, 7);
+	std::cout << "S5-OUTPUT" << std::endl;
+	std::cout << s5;
+	std::cout << "S3-OUTPUT" << std::endl;
+	std::cout << s3;
+
+	std::cout << "************** At[] ***************" << std::endl;
+	std::cout << "Pos 3: " << s5.at(3) << std::endl;
 	
 
-	/*s5.push_back('a');
+	std::cout << "************** Push-Pop ***************" << std::endl;
+	s5.push_back('a');
 	std::cout << s5;
 	s5.pop_back();
 	std::cout << s5;
 
 	std::cout << std::endl << "Erasing!" << std::endl;
 	s3.erase(3, 5);
-	std::cout << s3;*/
+	std::cout << s3;
 
 	return 0;
 }
