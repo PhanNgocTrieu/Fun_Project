3290b0e781400c528afd7a8ed858b3f63e5516bd
PhanNgocTrieu <47733751+PhanNgocTrieu@users.noreply.github.com>
2021-06-16 15:01:06 +0700

    Update sharedPtr.h

--- SmartPointer/sharedPtr.h
+++ SmartPointer/sharedPtr.h
@@ -1,127 +1,316 @@
 #ifndef SHAREDPTR_H
 #define SHAREDPTR_H
+#include <stdio.h>
 #include <iostream>
+#include <atomic>
+#include <memory>
+#include <thread>
+#include "weakPtr.h"
+using namespace std;
 
 template <class _Tvar>
 class mSharePtr
 {
 public:
+    friend class mWeakPtr;
     mSharePtr();
-    mSharePtr(_Tvar* ptr);
-    mSharePtr(const mSharePtr<_Tvar>& _other);
-
+    explicit mSharePtr(_Tvar *ptr);
+    mSharePtr(const mSharePtr<_Tvar> &_other);
     ~mSharePtr();
 
-    _Tvar* get();
-    _Tvar& operator->();
-    _Tvar& operator*();
+    mSharePtr &operator=(const mSharePtr<_Tvar> &_other);
+    mSharePtr &operator=(_Tvar *_other);
 
-    void reset();
+    _Tvar *get() const;
+    _Tvar *operator->() const;
+    _Tvar &operator*() const;
+    bool operator==(const mSharePtr &_other) const;
+    bool operator==(nullptr_t) const;
+    bool operator!=(const mSharePtr &_other) const;
+    bool operator!=(nullptr_t) const;
+    explicit operator bool() const noexcept {
+        return this->get() != nullptr;
+    }
 
+    int getRefCount() const;
 
-protected:
-    struct SharePtrCtrl
-    {
-        _Tvar *mPtrManage;
-        int mSharePtrCount;
-    };
+    void swap(mSharePtr& _other) noexcept;
+    void reset() noexcept;
 
-    SharePtrCtrl *mSharePtrCtrl;
+    template <class __Tvar>
+    friend ostream &operator<<(ostream &os, const mSharePtr<__Tvar> &obj);
 
-    mSharePtr& operator=(const mSharePtr& _other);
+private:
+    void cleanUp();
+    _Tvar *mPtr;
+    int *ref_count;
 };
 
+
+template <class _Tvar>
+atomic<int *> atmRefCount(mSharePtr<_Tvar>::ref_count);
+
+/**
+ *  @brief Construct an empty %shared_ptr.
+ *  @param getRefCount()==0 && get()==nullptr
+ */
 template <class _Tvar>
 mSharePtr<_Tvar>::mSharePtr()
+    : mPtr{nullptr}, ref_count{new int(0)} //ref_count{nullptr}
 {
-    printf("%d - %s \n",__LINE__, __FUNCTION__);
-    mSharePtrCtrl = nullptr;
+    printf("Line: %d - %s\n", __LINE__, __FUNCTION__);
 }
 
-template<class _Tvar>
-mSharePtr<_Tvar>::mSharePtr(_Tvar* ptr)
+/**
+ *  @brief  Construct a %shared_ptr that owns the pointer __ptr
+ *  @param  @a _mPtr A pointer that is convertible to element_type
+ *  @post   getRefcount() == 1 && get() == _ptr
+ *  @throw  std::bad_alloc, in which case @c delete @a __p is called.
+ */
+template <class _Tvar>
+mSharePtr<_Tvar>::mSharePtr(_Tvar *ptr) : mPtr{ptr}, ref_count{new int(0)}
 {
-    
-    mSharePtrCtrl = new SharePtrCtrl();
-    mSharePtrCtrl->mPtrManage = ptr;
-    mSharePtrCtrl->mSharePtrCount = 0;
-    if (mSharePtrCtrl->mPtrManage)
+    printf("Line: %d - %s(_Tvar * const ptr)\n", __LINE__, __FUNCTION__);
+    if (mPtr)
+        (*ref_count)++;
+}
+
+/**
+ *  @brief  if @a _other is empty, constructs an empth %share_ptr
+ *          otherwise construct a %share_ptr that shares ownership
+ *          with @a _other
+ *  @param  @a _mPtr A pointer that point to @a _other.
+ *  @post   _get() == _other.ref_count && _ get() == _other.mPtr (_other.get())
+ *   
+ */
+template <class _Tvar>
+mSharePtr<_Tvar>::mSharePtr(const mSharePtr<_Tvar> &_other)
+{
+    printf("%d - %s(const mSharePtr<_Tvar>& _other)\n", __LINE__, __FUNCTION__);
+    this->mPtr = _other.mPtr;
+    this->ref_count = _other.ref_count; // reference to _other.ref_count
+    if (this->mPtr)
     {
-        mSharePtrCtrl->mSharePtrCount++;
+        (*this->ref_count)++;
     }
-    printf("%d - %s \n",__LINE__, __FUNCTION__);
 }
 
-template<class _Tvar>
-mSharePtr<_Tvar>::mSharePtr(const mSharePtr<_Tvar>& _other)
+/**
+ *  @brief cleanUp _mPtr && _ref_count - rely on reference_counter - whether it's 0 or not
+ *  @param deallocated _mPtr && _ref_count
+ *  @post  delete _mPtr; delete ref_count  
+ */
+template <class _Tvar>
+mSharePtr<_Tvar>::~mSharePtr()
 {
-    std::cout << "Go here! Copy! " << std::endl;
-    printf("%d - %s \n",__LINE__, __FUNCTION__);
-    mSharePtrCtrl = new SharePtrCtrl;
-    mSharePtrCtrl->mPtrManage = _other.mSharePtrCtrl->mPtrManage;
-    mSharePtrCtrl->mSharePtrCount++;
-
+    printf("Line: %d - %s\n", __LINE__, __FUNCTION__);
+    cleanUp();
 }
 
-template<class T>
-mSharePtr<T>& mSharePtr<T>::operator=(const mSharePtr<T>& _other)
+/**
+ *  @brief  get value of Reference Counter
+ *  @param  getRefCount = (*this->ref_count)
+ *  @post   use_count() == (*this->ref_count)
+ *   
+ */
+template <class _Tvar>
+int mSharePtr<_Tvar>::getRefCount() const
 {
-    std::cout << "Go here! Assignment" << std::endl;
-    printf("%d - %s \n",__LINE__, __FUNCTION__);
-    mSharePtrCtrl->mPtrManage = _other.mSharePtrCtrl->mPtrManage;
-    mSharePtrCtrl->mSharePtrCount = _other.mSharePtrCtrl->mSharePtrCount;
-    mSharePtrCtrl->mSharePtrCount++;
-    
-    return *this;
+    //printf("Line: %d - %s\n", __LINE__, __FUNCTION__);
+    return (*ref_count);
 }
 
-template<class T>
-mSharePtr<T>::~mSharePtr()
+/**
+ *  @brief  get value of pointer which is managing
+ *  @param  return value of mPtr
+ *  @post   get() == *this->mPtr
+ *   
+ */
+template <class _Tvar>
+_Tvar *mSharePtr<_Tvar>::get() const
 {
-    printf("%d - %s \n",__LINE__, __FUNCTION__);
+    printf("Line: %d - %s\n", __LINE__, __FUNCTION__);
+    return mPtr;
+}
 
-    if (mSharePtrCtrl == nullptr)
+/**
+ *  @brief  operator-> overloading
+ *          return type must be a pointer or an object of a class to which you can apply.
+ *  @param  get() { return mPtr; }
+ *  @post   get() == this->mPtr
+ *   
+ */
+template <class _Tvar>
+_Tvar *mSharePtr<_Tvar>::operator->() const
+{
+    printf("Line: %d - %s\n", __LINE__, __FUNCTION__);
+    if (mPtr == nullptr || (*ref_count) == 0)
+        throw std::runtime_error("Invalid Pointer");
+    return mPtr;
+}
+
+/**
+ *  @brief  operator-> overloading
+ *          dereference 
+ *  @param  get() { return *mPtr; }
+ *  @post   get() == *this->mPtr
+ *   
+ */
+template <class _Tvar>
+_Tvar &mSharePtr<_Tvar>::operator*() const
+{
+    printf("Line: %d - %s\n", __LINE__, __FUNCTION__);
+    if (mPtr == nullptr || (*ref_count) == 0)
+        throw std::runtime_error("Invalid Pointer");
+    return *mPtr;
+}
+
+/**
+ *  @brief  operator== overloading.
+ *          it would be true if _mPtr == _other->_mPtr     
+ *  @param  _other sharePtr object
+ *  @param  _mPtr *this->_mPtr
+ *  @post   get() == *this->mPtr
+ *   
+ */
+template <class _Tvar>
+bool mSharePtr<_Tvar>::operator==(const mSharePtr &_other) const
+{
+    printf("Line: %d - %s(const mSharePtr& _other)\n", __LINE__, __FUNCTION__);
+    return (this->mPtr == _other.mPtr);
+}
+
+/**
+ *  @brief  operator== overloading.
+ * it would be true if _mPtr == nullptr
+ *           
+ *  @param  nullptr_t nullptr_type,
+ *  @param  _mPtr Ptr_manage
+ *   
+ */
+template <class _Tvar>
+bool mSharePtr<_Tvar>::operator==(nullptr_t) const
+{
+    printf("Line: %d - %s(nullptr_t)\n", __LINE__, __FUNCTION__);
+    return (mPtr == nullptr);
+}
+
+/**
+ *  @brief  operator!= overloading.
+ * it would be true if _mPtr != nullptr
+ *           
+ *  @param  nullptr_t nullptr_type,
+ *  @param  _mPtr Ptr_manage
+ *  
+ */
+template <class _Tvar>
+bool mSharePtr<_Tvar>::operator!=(nullptr_t) const
+{
+    printf("Line: %d - %s(nullptr_t)\n", __LINE__, __FUNCTION__);
+    return (mPtr != nullptr);
+}
+
+/**
+ *  @brief  operator!= overloading.
+ * it would be true if _mPtr != _other
+ *           
+ *  @param  _ohter __sharePtr_object,
+ *  @param  _mPtr internal __sharePtr_object
+ *  
+ */
+template <class _Tvar>
+bool mSharePtr<_Tvar>::operator!=(const mSharePtr &_other) const
+{
+    printf("Line: %d - %s(const mSharePtr& _other)\n", __LINE__, __FUNCTION__);
+    return (mPtr != _other.mPtr);
+}
+
+template <class __Tvar>
+ostream &operator<<(ostream &os, const mSharePtr<__Tvar> &obj)
+{
+    os << "Address: " << obj.mPtr << endl;
+    os << "Value Counter: " << (*obj.ref_count) << endl;
+    return os;
+}
+
+template <class _Tvar>
+mSharePtr<_Tvar> &mSharePtr<_Tvar>::operator=(const mSharePtr<_Tvar> &_other)
+{
+    cleanUp();
+
+    printf("%d - %s(_Tvar* ptr)\n", __LINE__, __FUNCTION__);
+    this->mPtr = _other.mPtr;
+    this->ref_count = _other.ref_count;
+    if (mPtr)
+    {
+        (*this->ref_count)++;
+    }
+
+    return this;
+}
+
+
+
+template <class _Tvar>
+mSharePtr<_Tvar> &mSharePtr<_Tvar>::operator=(_Tvar *_other)
+{
+    cleanUp();
+
+    this->mPtr = _other;
+    this->ref_count = new int(1);
+    if (this->mPtr)
+    {
+        (*ref_count)++;
+    }
+}
+
+/**
+ *  @brief  Deallocated memory space that we already allocated
+ *          Trying to deallocated all of share_ptr participate in ownership of the same source
+ *          Share_ptr Object would be delete when reference_counter would be 0.
+ *          Taking look what happen.
+ *  @param  _mPtr internal _sharePtr_Object
+ *  @param  ref_count reference counter
+ *  @result deallocated objects who participate in ownership of the same source. _mPtr == allocated if reference_counter == 0
+ *          
+ *  
+*/
+template <class _Tvar>
+void mSharePtr<_Tvar>::cleanUp()
+{
+    if (mPtr == nullptr)
         return;
-    
-    // count--
-    mSharePtrCtrl->mSharePtrCount--;
 
-    // checking for count == 0 -> delete
-    if (mSharePtrCtrl->mSharePtrCount <= 0)
+    printf("(ref_count != 0)\n");
+    (*ref_count)--;
+    if ((*ref_count) <= 0)
     {
-        printf("%d - %s \n",__LINE__, __FUNCTION__);
-        //delete prototype of mSharePtrControl
-        delete mSharePtrCtrl->mPtrManage;
+        printf("(ref_count == 0)\n");
 
-        //delete mSharePtrControl if all owners were already deleted 
-        delete mSharePtrCtrl;
+        if (mPtr)
+        {
+            printf("(mPtr != nullptr)\n");
+            delete mPtr;
+            mPtr = nullptr;
+        }
+        delete ref_count;
+        ref_count = nullptr;
     }
 }
 
-
 template<class _Tvar>
-_Tvar* mSharePtr<_Tvar>::get()
+void mSharePtr<_Tvar>::swap(mSharePtr& _other) noexcept
 {
-    if (mSharePtrCtrl == nullptr || mSharePtrCtrl->mSharePtrCount == 0)
-        return nullptr;
-    return mSharePtrCtrl->mPtrManage;
+    this->mPtr = move(_other.mPtr);
+    this->ref_count = move(_other.ref_count);
+
+    _other.~mSharePtr();
 }
 
 template<class _Tvar>
-_Tvar& mSharePtr<_Tvar>::operator->()
-{
-    if (mSharePtrCtrl == nullptr || mSharePtrCtrl->mSharePtrCount == 0)
-        throw std::runtime_error("Invalid Pointer");
-    return *mSharePtrCtrl;
+void mSharePtr<_Tvar>::reset() noexcept 
+{   // release resource and convert to empty shared_ptr object
+    mSharePtr().swap(*this);
 }
-
-template<class _Tvar>
-_Tvar& mSharePtr<_Tvar>::operator*()
-{
-    if (mSharePtrCtrl == nullptr || mSharePtrCtrl->mSharePtrCount == 0)
-        throw std::runtime_error("Invalid Pointer");
-    return &mSharePtrCtrl;
-}
-
 
 #endif
