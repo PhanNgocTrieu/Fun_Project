e84513e6f47410e39217027b1154343e72d4f332
PhanNgocTrieu <bettertogetheronit@gmail.com>
2021-07-06 19:14:19 +0700

    Update SOCKET_LINUX

--- SOCKET_LINUX/Client.cpp
+++ SOCKET_LINUX/Client.cpp
@@ -8,6 +8,7 @@
 #include <netinet/in.h>
 #include <errno.h>
 #include <arpa/inet.h>
+#include <vector>
 #include <iostream>
 
 using namespace std;
@@ -31,35 +32,154 @@ struct message_header
     long int timestamp; // epoch time in milisecond
     int length_of_data;
 };
-struct message_ls
+
+vector<string> split(char *_string)
 {
-    // struct message_header header;
-    // // The data here
-    string msgToServer;
-    int lenOfMessage;
-};
-struct message_dwn
+    vector<string> res;
+    string s = "";
+    char *pchar = _string;
+    int limit = strlen(_string) - 1;
+    while (pchar != nullptr && limit > 0)
+    {
+        if (*pchar != ' ')
+        {
+            s += *pchar;
+        }
+        else
+        {
+            res.push_back(s);
+            s = "";
+        }
+        pchar++;
+        limit--;
+    }
+
+    if (s.size() != 0)
+    {
+        cout << "\ns : " << s << endl;
+        cout << "len: " << s.length() << endl;
+        res.push_back(s);
+    }
+
+    return res;
+}
+// void check()
+// {
+//     printf("\nSize of header          = %ld bytes", sizeof(struct message_header));
+//     printf("\nSize of preamble        = %ld", sizeof(msg.preamble));
+//     printf("\nSize of msg_class       = %ld", sizeof(msg.msg_class));
+//     printf("\nSize of msg_type        = %ld", sizeof(msg.msg_type));
+//     printf("\nSize of timestamp       = %ld", sizeof(msg.timestamp));
+//     printf("\nSize of length_of_data  = %ld", sizeof(msg.length_of_data));
+
+//     printf("\n\nValue of preamble       	= %d", msg.preamble);
+//     printf("\nValue of msg_class        = %d", msg.msg_class);
+//     printf("\nValue of msg_type         = %d", msg.msg_type);
+//     printf("\nValue of timestamp        = %ld", msg.timestamp);
+//     printf("\nValue of length_of_data   = %d", msg.length_of_data);
+
+//     printf("\n\nAddress of preamble      	= %p", &msg.preamble);
+//     printf("\nAddress of msg_class       = %p", &msg.msg_class);
+//     printf("\nAddress of msg_type        = %p", &msg.msg_type);
+//     printf("\nAddress of timestamp       = %p", &msg.timestamp);
+//     printf("\nAddress of length_of_data  = %p", &msg.length_of_data);
+// }
+
+void DownloadFile(int sockfd, char *data_to_send, message_header msg, vector<string> splitString)
 {
-    struct message_header header;
-    // The data here
-    char *fileSave;
-    char *location;
-};
+    char *fileName;
+    fileName = (char *)stringforFile[1].c_str();
+    char *localPath;
+    localPath = (char *)stringforFile[2].c_str();
+
+    msg.preamble = 12343;
+    msg.msg_class = e_msg_class_request;
+    msg.msg_type = e_msg_type_get;
+    msg.timestamp = 112324;
+
+    if (sockfd < 0)
+    {
+        return;
+    }
+    while (1)
+    {
+        if (fileName[0] == '.')
+        {
+            send(sockfd, fileName, sizeof(fileName), 0);
+            break;
+        }
+        if (localPath[0] == '.')
+        {
+            send(sockfd, localPath, sizeof(localPath), 0);
+            break;
+        }
+        msg.length_of_data = strlen(fileName);
+
+        data_to_send = (char *)malloc(sizeof(struct message_header) + msg.length_of_data);
+
+        // setup for sending fileName;
+        int idex = 0;
+        memcpy(data_to_send + idex, &msg.preamble, sizeof(msg.preamble));
+        idex += sizeof(msg.preamble);
+        memcpy(data_to_send + idex, &msg.msg_class, sizeof(msg.msg_class));
+        idex += sizeof(msg.msg_class);
+        memcpy(data_to_send + idex, &msg.msg_type, sizeof(msg.msg_type));
+        idex += sizeof(msg.msg_type);
+        memcpy(data_to_send + idex, &msg.timestamp, sizeof(msg.timestamp));
+        idex += sizeof(msg.timestamp);
+        memcpy(data_to_send + idex, &msg.length_of_data, sizeof(msg.length_of_data));
+        memcpy(data_to_send + sizeof(message_header), fileName, msg.length_of_data);
+
+        printf("\nTotal data to send: %ld bytes", sizeof(struct message_header) + msg.length_of_data);
+        send(sockfd, fileName, sizeof(fileName), 0);
+        char GotFileSize[1024];
+        recv(sockfd, GotFileSize, 1024, 0);
+
+        // taking size of File in integer
+        long FileSize = atoi(GotFileSize);
+        long SizeCheck = 0;
+
+        // open local file that we will store;
+        FILE *fp = fopen(localPath.c_str(), "w");
+        char *mfcc;
+        if (FileSize > 1499)
+        {
+            // maximun is 1500 bytes
+            mfcc = (char *)malloc(1500);
+            while (SizeCheck < FileSize)
+            {
+                int Received = recv(sockfd, mfcc, 1500, 0);
+                SizeCheck += Received;
+                fwrite(mfcc, 1, Received, fp);
+                fflush(fp);
+                printf("Filesize: %ld\nSizecheck: %ld\nReceived: %d\n\n", FileSize, SizeCheck, Received);
+            }
+        }
+        else
+        {
+            mfcc = (char *)malloc(FileSize + 1);
+            int Received = recv(sockfd, mfcc, FileSize, 0);
+            fwrite(mfcc, 1, Received, fp);
+            fflush(fp);
+        }
+        fclose(fp);
+        sleep(500);
+        free(mfcc);
+    }
+}
 
 int main(int argc, char *argv[])
 {
     int sockfd, portno, readVal;
     int portNum;
     struct sockaddr_in serv_addr; //Cau truc chua dia chi server ma client can biet de ket noi to
+    message_header msg;
 
-    message_ls *msgls = new message_ls;
-    string readFromSever;
-    string command;
-
+    char msgToServer[100];
     char *data_to_send;
-    char *buffer = new char[1024];
-    memset(buffer, 0, 1024);
+    char *buffer;
 
+    FILE *fp;
     //Client nhan tham so hostname va port tu dong lenh
     if (argc < 3)
     {
@@ -88,24 +208,113 @@ int main(int argc, char *argv[])
     }
 
     cout << "Message/Command: ";
-    getline(cin, msgls->msgToServer);
+    fgets(msgToServer, 100, stdin);
 
-    msgls->lenOfMessage = msgls->msgToServer.length();
-    cout << "Message to send:\n";
-    cout << "\tMessage: " << msgls->msgToServer << endl;
-    cout << "\tLen: " << msgls->lenOfMessage << endl;
-    int statusSend = send(sockfd, (char *)msgls, sizeof(e_msg_type_ls) + msgls->lenOfMessage, 0);
-    if (statusSend < 0)
+    vector<string> splitString = split(msgToServer);
+    if (splitString.size() != 0)
     {
-        perror("Error: Sending failed");
+        if (splitString[0] == "ls")
+        {
+            msg.length_of_data = splitString[1].length();
+            cout << "Message to send:\n";
+            cout << "\tMessage: " << splitString[1] << endl;
+            cout << "\tLen: " << splitString[1].length() << endl;
+
+            msg.preamble = 1111;
+            msg.msg_type = e_msg_type_ls;
+            msg.timestamp = 123456789;
+
+            data_to_send = (char *)malloc(sizeof(struct message_header) + msg.length_of_data);
+            printf("\nTotal data to send: %ld bytes", sizeof(struct message_header) + msg.length_of_data);
+
+            int idex = 0;
+            memcpy(data_to_send + idex, &msg.preamble, sizeof(msg.preamble));
+            idex += sizeof(msg.preamble);
+            memcpy(data_to_send + idex, &msg.msg_class, sizeof(msg.msg_class));
+            idex += sizeof(msg.msg_class);
+            memcpy(data_to_send + idex, &msg.msg_type, sizeof(msg.msg_type));
+            idex += sizeof(msg.msg_type);
+            memcpy(data_to_send + idex, &msg.timestamp, sizeof(msg.timestamp));
+            idex += sizeof(msg.timestamp);
+            memcpy(data_to_send + idex, &msg.length_of_data, sizeof(msg.length_of_data));
+            memcpy(data_to_send + sizeof(struct message_header), (char *)splitString[1].c_str(), msg.length_of_data);
+
+            int statusSend = send(sockfd, data_to_send, sizeof(struct message_header) + msg.length_of_data, 0);
+            if (statusSend < 0)
+            {
+                perror("Error: Sending failed");
+            }
+            else
+            {
+                //cout << "Already send!" << endl;
+                printf("\nClient send %d bytes\n", statusSend);
+            }
+        }
+
+        if (splitString[0] == "rm")
+        {
+            msg.length_of_data = splitString[1].length();
+            cout << "Message to send:\n";
+            cout << "\tMessage: " << splitString[1] << endl;
+            cout << "\tLen: " << splitString[1].length() << endl;
+
+            msg.preamble = 1111;
+            msg.msg_type = e_msg_type_rm;
+            msg.timestamp = 123456789;
+
+            data_to_send = (char *)malloc(sizeof(struct message_header) + msg.length_of_data);
+            printf("\nTotal data to send: %ld bytes", sizeof(struct message_header) + msg.length_of_data);
+
+            int idex = 0;
+            memcpy(data_to_send + idex, &msg.preamble, sizeof(msg.preamble));
+            idex += sizeof(msg.preamble);
+            memcpy(data_to_send + idex, &msg.msg_class, sizeof(msg.msg_class));
+            idex += sizeof(msg.msg_class);
+            memcpy(data_to_send + idex, &msg.msg_type, sizeof(msg.msg_type));
+            idex += sizeof(msg.msg_type);
+            memcpy(data_to_send + idex, &msg.timestamp, sizeof(msg.timestamp));
+            idex += sizeof(msg.timestamp);
+            memcpy(data_to_send + idex, &msg.length_of_data, sizeof(msg.length_of_data));
+            memcpy(data_to_send + sizeof(struct message_header), (char *)splitString[1].c_str(), msg.length_of_data);
+
+            int statusSend = send(sockfd, data_to_send, sizeof(struct message_header) + msg.length_of_data, 0);
+            if (statusSend < 0)
+            {
+                perror("Error: Sending failed");
+            }
+            else
+            {
+                //cout << "Already send!" << endl;
+                printf("\nClient send %d bytes\n", statusSend);
+            }
+        }
+
+        if (splitString[0] == "download")
+        {
+
+            DownloadFile(sockfd, data_to_send, msg, splitString);
+        }
+
+        string list;
+        buffer = new char[1024];
+        readVal = read(sockfd, buffer, 1024);
+        if (readVal <= 0)
+        {
+            perror("Error: could not read fom client!\n");
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            printf("\nServer read %d bytes\n", readVal);
+        }
+
+        char msgFromServer[1024];
+
+        printf("\nMessage from client: \n%s\n", buffer);
+
+        delete buffer;
+        buffer = nullptr;
     }
-    else
-        cout << "Already send!" << endl;
-    //read(sockfd,buffer,sizeof(buffer));
-
-    recv(sockfd, buffer, 1024, 0);
-    cout << buffer << endl;
-
     close(sockfd); //Dong socket
     return 0;
 }
--- SOCKET_LINUX/Server.cpp
+++ SOCKET_LINUX/Server.cpp
@@ -12,6 +12,7 @@
 #include <vector>
 #include <iostream>
 
+
 using namespace std;
 
 string readFiles(const string &dirPath)
@@ -24,7 +25,6 @@ string readFiles(const string &dirPath)
     {
         while ((ent = readdir(dir)) != nullptr)
         {
-            cout << "file: " << ent->d_name << " ";
             res += ent->d_name;
             res += " ";
         }
@@ -43,7 +43,9 @@ string removeDirOrFile(const string &path)
     int status;
     status = remove(path.c_str());
     if (status == 0)
+    {
         return string("File deleted successfully\n");
+    }
     return string("Error deleting!\n");
 }
 
@@ -83,13 +85,96 @@ vector<string> split(char *_string)
     return res;
 }
 
-struct message_ls
+enum e_msg_class
 {
-    // struct message_header header;
-    // // The data here
-    string msgToServer;
-    int lenOfMessage;
+    e_msg_class_request,
+    e_msg_class_response
 };
+enum e_msg_type
+{
+    e_msg_type_ls = 0,
+    e_msg_type_rm,
+    e_msg_type_get
+};
+struct message_header
+{
+    int preamble; //=0xAA55AA55
+    e_msg_class msg_class;
+    enum e_msg_type msg_type;
+    long int timestamp; // epoch time in milisecond
+    int length_of_data;
+};
+
+// void checking()
+// {
+//     printf("\n\nAddress of preamble       = %p", &recmsg.preamble);
+//     printf("\nAddress of msg_class        = %p", &recmsg.msg_class);
+//     printf("\nAddress of msg_type         = %p", &recmsg.msg_type);
+//     printf("\nAddress of timestamp        = %p", &recmsg.timestamp);
+//     printf("\nAddress of length_of_data   = %p", &recmsg.length_of_data);
+
+//     printf("\n\nValue of preamble           = %d", recmsg.preamble);
+//     printf("\nValue of msg_class          = %d", recmsg.msg_class);
+//     printf("\nValue of msg_type           = %d", recmsg.msg_type);
+//     printf("\nValue of timestamp          = %ld", recmsg.timestamp);
+//     printf("\nValue of length_of_data     = %d", recmsg.length_of_data);
+//     printf("\nMessage from client         = %s\n", msgFromClient);
+// }
+
+string getString(char *const &_String, int size)
+{
+    string s = "";
+    for (int idex = 0; idex < size; idex++)
+    {
+        s += _String[idex];
+    }
+    return s;
+}
+
+void downloadFile(int sockfd, message_header msg, char * buff)
+{
+    if (sockfd < 0)
+    {
+        return;
+    }
+
+    while (1)
+    {
+        char * fileName;
+        memcpy(fileName, buff + sizeof(message_header), msg.length_of_data);
+        recv(sockfd, fileName, sizeof(fileName),0);
+        if (fileName[0] == '.')
+        {
+            break;
+        }
+        FILE* fp = fopen(fileName, "r");
+		fseek(fp, 0, SEEK_END);
+		long FileSize = ftell(fp);
+		char GotFileSize[1024];
+		snprintf(GotFileSize, 10, "%ld", FileSize);
+		send(sockfd, GotFileSize, 1024, 0);
+		rewind(fp);
+		long SizeCheck = 0;
+		char* mfcc;
+		if(FileSize > 1499){
+			mfcc = (char*)malloc(1500);
+			while(SizeCheck < FileSize){
+				int Read = fread(mfcc, 1500, sizeof(char), fp);
+				int Sent = send(sockfd, mfcc, Read, 0);
+				SizeCheck += Sent;
+			}
+		}
+		else{
+			mfcc = (char*)malloc(FileSize + 1);
+			fread(mfcc, FileSize, sizeof(char), fp);
+			send(sockfd, mfcc, FileSize, 0);
+		}
+        fclose(fp);
+        sleep(500);
+        free(mfcc);
+    }
+    
+}
 
 int main(int argc, char const *argv[])
 {
@@ -99,10 +184,9 @@ int main(int argc, char const *argv[])
     int opt = 1;
     int addrlen = sizeof(address);
     int PORT;
-    message_ls *msgFromClient;
 
     // Communicating from Server and Client variables;
-    char *commu;
+    char *data_to_send;
     vector<string> token;
 
     if (argc < 2)
@@ -139,38 +223,105 @@ int main(int argc, char const *argv[])
 
     if (listen(server_fd, 10) < 0)
     {
-        perror("listen");
+        perror("listen failed");
         exit(EXIT_FAILURE);
     }
 
     if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
     {
-        perror("accept");
+        perror("accept failed");
         exit(EXIT_FAILURE);
     }
 
-    printf("Server is listening....\n");
+    printf("\nServer is listening....");
     char *buff = new char[1024];
-    string buffer = "";
+    char msgFromClient[100];
+    e_msg_type typeOfMessage;
+    string stringMessage;
     read_Val = read(new_socket, buff, 1024);
 
-    //read_Val = read(new_socket, buff, 1024);
     if (read_Val <= 0)
     {
         perror("Error: could not read fom client!\n");
         exit(EXIT_FAILURE);
     }
-    msgFromClient = new message_ls[sizeof(buff)];
-    memcpy(msgFromClient, (message_ls *)buff, sizeof(buff));
-    cout << "From Client:\n";
-    cout << "\tMessage: " << msgFromClient->msgToServer << endl;
-    cout << "\tlengthOfData: " << msgFromClient->lenOfMessage << endl;
-    cout << "\tAdrress buff " << &buff << endl;
-    cout << "\tValue buff: " << buff << endl;
-    const char *receive = "Already Received!";
-    send(new_socket, receive, strlen(receive), 0);
+    else
+    {
+        printf("\nServer read %d bytes\n", read_Val);
+    }
+    message_header recmsg;
+    memcpy(&typeOfMessage, buff + sizeof(recmsg.preamble) + sizeof(recmsg.msg_class), sizeof(recmsg.msg_type));
 
-  
+    if (typeOfMessage == e_msg_type_ls)
+    {
+        int index = 0;
+        memcpy(&recmsg.preamble, buff + index, sizeof(recmsg.preamble));
+        index += sizeof(recmsg.preamble);
+        memcpy(&recmsg.msg_class, buff + index, sizeof(recmsg.msg_class));
+        index += sizeof(recmsg.msg_class);
+        memcpy(&recmsg.msg_type, buff + index, sizeof(recmsg.msg_type));
+        index += sizeof(recmsg.msg_type); // xem phan address
+        memcpy(&recmsg.timestamp, buff + index, sizeof(recmsg.timestamp));
+        index += sizeof(recmsg.timestamp);
+        memcpy(&recmsg.length_of_data, buff + index, sizeof(recmsg.length_of_data));
+        memcpy(&msgFromClient, buff + sizeof(struct message_header), recmsg.length_of_data);
+        msgFromClient[recmsg.length_of_data + 1] = '\n';
+
+        stringMessage = getString(msgFromClient, recmsg.length_of_data);
+
+        string filePath = readFiles(stringMessage);
+        cout << "\tCommand: " << stringMessage << endl;
+        cout << "\tlen of path: " << stringMessage.length() << endl;
+        cout << "\tMessage: " << filePath << endl;
+        int statusSend = send(new_socket, (char *)filePath.c_str(), filePath.length(), 0);
+        if (statusSend < 0)
+        {
+            perror("Error: Sending failed");
+        }
+        else
+        {
+            //cout << "Already send!" << endl;
+            printf("\nClient send %d bytes\n", statusSend);
+        }
+    }
+
+    if (typeOfMessage == e_msg_type_rm)
+    {
+        int index = 0;
+        memcpy(&recmsg.preamble, buff + index, sizeof(recmsg.preamble));
+        index += sizeof(recmsg.preamble);
+        memcpy(&recmsg.msg_class, buff + index, sizeof(recmsg.msg_class));
+        index += sizeof(recmsg.msg_class);
+        memcpy(&recmsg.msg_type, buff + index, sizeof(recmsg.msg_type));
+        index += sizeof(recmsg.msg_type); // xem phan address
+        memcpy(&recmsg.timestamp, buff + index, sizeof(recmsg.timestamp));
+        index += sizeof(recmsg.timestamp);
+        memcpy(&recmsg.length_of_data, buff + index, sizeof(recmsg.length_of_data));
+        memcpy(&msgFromClient, buff + sizeof(struct message_header), recmsg.length_of_data);
+        msgFromClient[recmsg.length_of_data + 1] = '\n';
+
+        stringMessage = getString(msgFromClient, recmsg.length_of_data);
+
+        string message = removeDirOrFile(stringMessage);
+        cout << "\tCommand: " << stringMessage << endl;
+        cout << "\tlen of path: " << message.length() << endl;
+        cout << "\tMessage: " << message << endl;
+        int statusSend = send(new_socket, (char *)message.c_str(), message.length(), 0);
+        if (statusSend < 0)
+        {
+            perror("Error: Sending failed");
+        }
+        else
+        {
+            //cout << "Already send!" << endl;
+            printf("\nClient send %d bytes\n", statusSend);
+        }
+    }
+
+    if (typeOfMessage == e_msg_type_get)
+    {
+        downloadFile(new_socket, recmsg, buff);
+    }
 
     close(new_socket);
     close(server_fd);
